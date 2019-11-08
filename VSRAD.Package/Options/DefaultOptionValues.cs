9f21b16f0a9f5c41d2fd0ea2d2f5c8389f2cbbb0
johnny-keker <giomail.iv@gmail.com>
2019-11-08 06:45:06 -0800

    working directory default values

--- VSRAD.Package/Options/DefaultOptionValues.cs
+++ VSRAD.Package/Options/DefaultOptionValues.cs
@@ -6,7 +6,7 @@ namespace VSRAD.Package.Options
     internal static class DefaultOptionValues
     {
         #region General
-        public const string DeployDirectory = "$(" + RadMacros.DebuggerWorkingDirectory + ")";
+        public const string DeployDirectory = "";
         public const string RemoteMachineAdredd = "127.0.0.1";
         public const int Port = 9339;
         public const DocumentSaveType AutosaveSource = DocumentSaveType.ActiveDocument;
@@ -16,7 +16,7 @@ namespace VSRAD.Package.Options
         #region Debugger
         public const string DebuggerExecutable = "python.exe";
         public const string DebuggerArguments = "script.py -w $(" + RadMacros.Watches + ") -l $(" + RadMacros.BreakLine + ") -v \"$(" + RadMacros.DebugAppArgs + ")\" -t $(" + RadMacros.Counter + ") -p \"$(" + RadMacros.DebugBreakArgs + ")\" -f \"$(" + RadMacros.ActiveSourceFile + ")\" -o \"$(" + RadMacros.DebuggerOutputPath + ")\"";
-        public const string DebuggerWorkingDirectory = "";
+        public const string DebuggerWorkingDirectory = "$(" + RadMacros.DeployDirectory + ")";
         public const string DebuggerOutputPath = "";
         public const bool DebuggerBinaryOutput = false;
         public const bool DebuggerParseValidWatches = false;
@@ -28,7 +28,7 @@ namespace VSRAD.Package.Options
         #region Disassembler
         public const string DisassemblerExecutable = "";
         public const string DisassemblerArguments = "";
-        public const string DisassemblerWorkingDirectory = "";
+        public const string DisassemblerWorkingDirectory = "$(" + RadMacros.DeployDirectory + ")";
         public const string DisassemblerOutputPath = "";
         public const string DisassemblerLocalOutputCopyPath = "";
         public const string DisassemblerLineMaker = "";
@@ -36,7 +36,7 @@ namespace VSRAD.Package.Options
         #region Profiler
         public const string ProfilerExecutable = "";
         public const string ProfilerArguments = "";
-        public const string ProfilerWorkingDirectory = "";
+        public const string ProfilerWorkingDirectory = "$(" + RadMacros.DeployDirectory + ")";
         public const string ProfilerOutputPath = "";
         public const string ProfilerViewerExecutable = "";
         public const string ProfilerViewerArguments = "";
