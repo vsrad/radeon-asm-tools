f0d183cfb8fbd3ea3fe27135618cb42da6985c80
johnny-keker <giomail.iv@gmail.com>
2019-11-08 11:18:44 -0800

    save user json test

--- VSRAD.PackageTests/Server/FileSynchronizationManagerTests.cs
+++ VSRAD.PackageTests/Server/FileSynchronizationManagerTests.cs
@@ -238,5 +238,35 @@ namespace VSRAD.PackageTests.Server
             File.Delete(additionaFilePathFirst);
             File.Delete(additionaFilePathSecond);
         }
+
+        [Fact]
+        public async Task SaveUserJsonOnRunAsync()
+        {
+            int saveOptionsCount = 0;
+            TestHelper.InitializePackageTaskFactory();
+            var channel = new MockCommunicationChannel();
+            var events = new Mock<IProjectEvents>();
+            var packer = new Mock<IDeployFilePacker>();
+            var project = new Mock<IProject>(MockBehavior.Strict);
+            project.Setup((p) => p.RootPath).Returns(_projectRoot);
+            project.Setup((p) => p.SaveOptions()).Callback(() => saveOptionsCount++);
+            var projectSourceManager = new Mock<IProjectSourceManager>();
+            var evaluator = new Mock<IMacroEvaluator>(MockBehavior.Strict);
+            evaluator.Setup((e) => e.GetMacroValueAsync(RadMacros.DeployDirectory)).Returns(Task.FromResult(_deployDirectory));
+            project.Setup((p) => p.GetMacroEvaluatorAsync(It.IsAny<uint>(), It.IsAny<string[]>())).Returns(Task.FromResult(evaluator.Object));
+            var options = new ProjectOptions();
+            options.AddProfile("Default", new ProfileOptions());
+            project.Setup((p) => p.Options).Returns(options);
+
+            var deployManager = new FileSynchronizationManager(channel.Object, packer.Object, events.Object, project.Object, projectSourceManager.Object);
+
+            channel.ThenExpect<DebugServer.IPC.Commands.Deploy>();
+            await deployManager.SynchronizeRemoteAsync();
+            Assert.Equal(1, saveOptionsCount);
+
+            channel.ThenExpect<DebugServer.IPC.Commands.Deploy>();
+            await deployManager.SynchronizeRemoteAsync();
+            Assert.Equal(2, saveOptionsCount);
+        }
     }
 }
