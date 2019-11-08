08e7085f4d75afa53f716455e3f2e5a9394dfd7e
timlathy <tim.labushev@gmail.com>
2019-11-08 14:17:54 +0300

    build tools: handle ipc pipe timeout

--- VSRAD.BuildTools/RemoteBuildTask.cs
+++ VSRAD.BuildTools/RemoteBuildTask.cs
@@ -1,20 +1,37 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System;
 
 namespace VSRAD.BuildTools
 {
     public sealed class RemoteBuildTask : Task
     {
+        public const string TimeoutError = "Failed to connect to the IDE build server. Ensure the project you're attempting to build is open in Visual Studio.";
+
         [Required]
         public string ProjectDir { get; set; }
 
         public override bool Execute()
         {
             var bridge = new IPCBridge(ProjectDir);
-            var result = bridge.Build();
 
-            foreach (var message in RemoteBuildStderrParser.ExtractMessages(result.Stderr))
+            try
             {
+                var result = bridge.Build();
+
+                LogBuildResult(result);
+                return result.ExitCode == 0;
+            }
+            catch (TimeoutException)
+            {
+                Log.LogError(TimeoutError);
+                return false;
+            }
+        }
+
+        private void LogBuildResult(IPCBuildResult result)
+        {
+            foreach (var message in RemoteBuildStderrParser.ExtractMessages(result.Stderr))
                 switch (message.Kind)
                 {
                     case RemoteBuildStderrParser.MessageKind.Error:
@@ -32,11 +49,8 @@ namespace VSRAD.BuildTools
                             endLineNumber: 0, endColumnNumber: 0);
                         break;
                 }
-            }
 
             Log.LogMessage(MessageImportance.High, $"Build finished with exit code {result.ExitCode}");
-
-            return result.ExitCode == 0;
         }
     }
 }
---
+++ VSRAD.BuildToolsTests/RemoteBuildTaskTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using Moq;
+using VSRAD.BuildTools;
+using Xunit;
+
+namespace VSRAD.BuildToolsTests
+{
+    public class RemoteBuildTaskTests
+    {
+        [Fact]
+        public void TimeoutTest()
+        {
+            var engine = new Mock<IBuildEngine>();
+            var task = new RemoteBuildTask
+            {
+                ProjectDir = "/home/iwlain",
+                BuildEngine = engine.Object
+            };
+            task.Execute();
+
+            engine.Verify((e) => e.LogErrorEvent(It.Is<BuildErrorEventArgs>((a) => a.Message == RemoteBuildTask.TimeoutError)), Times.Once);
+        }
+    }
+}
