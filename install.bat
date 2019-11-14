56e6ead0fccb6179ea074ab7da0cbc01dd0b3d95
Slimakanzer <gleb.larochkin@gmail.com>
2019-11-14 15:52:47 +0300

    Improved install.bat and release.bat: override existing files

--- install.bat
+++ install.bat
@@ -1,17 +1,5 @@
-if exist "%LOCALAPPDATA%\CustomProjectSystems\ArthurProject" (
-    rd "%LOCALAPPDATA%\CustomProjectSystems\ArthurProject" /s /q
-)
-
-if exist "%LOCALAPPDATA%\CustomProjectSystems\GCNProject" (
-    rd "%LOCALAPPDATA%\CustomProjectSystems\GCNProject" /s /q
-)
-
-if not exist "%LOCALAPPDATA%\CustomProjectSystems\RADProject" (
-    mkdir "%LOCALAPPDATA%\CustomProjectSystems\RADProject"
-) else (
-    rd "%LOCALAPPDATA%\CustomProjectSystems\RADProject" /s /q
-    mkdir "%LOCALAPPDATA%\CustomProjectSystems\RADProject"
-)
+if exist "%LOCALAPPDATA%\CustomProjectSystems\RADProject" rd "%LOCALAPPDATA%\CustomProjectSystems\RADProject" /s /q
+mkdir "%LOCALAPPDATA%\CustomProjectSystems\RADProject"
 xcopy /E RADProject "%LOCALAPPDATA%\CustomProjectSystems\RADProject" /Y
 if not exist "%LOCALAPPDATA%\CustomProjectSystems\RADProject\Tools" mkdir "%LOCALAPPDATA%\CustomProjectSystems\RADProject\Tools"
 xcopy /E DebugServerW64 "%LOCALAPPDATA%\CustomProjectSystems\RADProject\Tools" /Y
--- release.bat
+++ release.bat
@@ -6,18 +6,18 @@ dotnet publish -r linux-x64 -c release --self-contained false
 cd ..
 :: DebugServer
 mkdir Release
-xcopy /E "VSRAD.DebugServer\bin\release\netcoreapp2.2\win-x64\publish" "Release\DebugServerW64\"
-xcopy /E "VSRAD.DebugServer\bin\release\netcoreapp2.2\linux-x64\publish" "Release\DebugServerLinux64\"
+xcopy /E /Y "VSRAD.DebugServer\bin\release\netcoreapp2.2\win-x64\publish" "Release\DebugServerW64\"
+xcopy /E /Y "VSRAD.DebugServer\bin\release\netcoreapp2.2\linux-x64\publish" "Release\DebugServerLinux64\"
 :: RadeonAsmDebugger.vsix
-copy "VSRAD.Package\bin\Release\RadeonAsmDebugger.vsix" "Release\"
+copy /Y "VSRAD.Package\bin\Release\RadeonAsmDebugger.vsix" "Release\"
 :: RadeonAsmSyntax.vsix
-copy "VSRAD.Syntax\bin\Release\RadeonAsmSyntax.vsix" "Release\"
+copy /Y "VSRAD.Syntax\bin\Release\RadeonAsmSyntax.vsix" "Release\"
 :: Changelog and Readme
-copy "CHANGELOG.md" "Release\"
-copy "README.md" "Release\"
+copy /Y "CHANGELOG.md" "Release\"
+copy "/Y README.md" "Release\"
 :: VSGRAD.BuildTools.dll
-copy "VSRAD.BuildTools\bin\Release\VSRAD.BuildTools.dll" "Release\"
+copy /Y "VSRAD.BuildTools\bin\Release\VSRAD.BuildTools.dll" "Release\"
 :: RADProject
-xcopy /E "%LOCALAPPDATA%\CustomProjectSystems\RADProject" "Release\RADProject\"
+xcopy /E /Y "%LOCALAPPDATA%\CustomProjectSystems\RADProject" "Release\RADProject\"
 :: install.bat
 copy "install.bat" "Release\"
