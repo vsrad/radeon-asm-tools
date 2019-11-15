85ad099b7a02d715c165a29e205ee6340d86a261
Slimakanzer <gleb.larochkin@gmail.com>
2019-11-15 14:39:37 +0300

    Syntax: basic error highlighting

---
+++ VSRAD.Syntax/SyntaxHighlighter/ErrorHighlighter/ErrorHighliterTagger.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace VSRAD.Syntax.SyntaxHighlighter.IdentifiersHighliter
+{
+    internal class ErrorSpanTag : TextMarkerTag
+    {
+        private static string GetColorFormat()
+        {
+            return "syntax error";
+        }
+
+        public ErrorSpanTag() : base(GetColorFormat()) { }
+    }
+
+    internal class ErrorHighlighterTagger : ITagger<ErrorSpanTag>
+    {
+        private readonly ITextView view;
+        private readonly ITextBuffer buffer;
+        private readonly object updateLock;
+        private NormalizedSnapshotSpanCollection ErorSpans { get; set; }
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        internal ErrorHighlighterTagger(ITextView textView, ITextBuffer sourceBuffer)
+        {
+            view = textView;
+            buffer = sourceBuffer;
+            updateLock = new object();
+
+            view.Caret.PositionChanged += CaretPositionChanged;
+            ErorSpans = new NormalizedSnapshotSpanCollection();
+        }
+
+        private void CaretPositionChanged(object sender, CaretPositionChangedEventArgs e) => UpdateErorMarker();
+
+        private void UpdateErorMarker()
+        {
+            ThreadPool.QueueUserWorkItem(UpdateSpanAdornments);
+        }
+
+        private void UpdateSpanAdornments(object threadContext)
+        {
+            SynchronousUpdate();
+        }
+
+        private void SynchronousUpdate()
+        {
+            lock (updateLock)
+            {
+                TagsChanged?.Invoke(this, new SnapshotSpanEventArgs(new SnapshotSpan(buffer.CurrentSnapshot, 0, buffer.CurrentSnapshot.Length)));
+            }
+        }
+
+        public IEnumerable<ITagSpan<ErrorSpanTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            var currentWord = new SnapshotSpan(buffer.CurrentSnapshot, 0, 4);
+            yield return new TagSpan<ErrorSpanTag>(currentWord, new ErrorSpanTag());
+        }
+    }
+}
