457fc1b7c46a174f48bde1405a94250dce19b781
johnny-keker <giomail.iv@gmail.com>
2019-11-15 11:53:25 -0800

    host source path mapping test passed

--- VSRAD.BuildToolsTests/Errors/LineMapperTests.cs
+++ VSRAD.BuildToolsTests/Errors/LineMapperTests.cs
@@ -45,6 +45,23 @@ int main() {
 }
 ";
 
+        public static readonly string[] ProjectPaths = new[] {
+            @"code/EVA00/main.c",
+            @"code/EVA00/sensors_controller.c",
+            @"code/EVA00/pilot_controller.c",
+            @"code/EVA00/motors_controller.c",
+            @"code/EVA00/battery_controller.c",
+            @"code/EVA00/connection_controller.c",
+            @"code/EVA01/main.c",
+            @"code/EVA01/sensors_controller.c",
+            @"code/EVA01/pilot_controller.c",
+            @"code/EVA01/motors_controller.c",
+            @"code/EVA01/battery_controller.c",
+            @"code/EVA01/connection_controller.c",
+        };
+
+        public const string RemotePath = @"MAIN_MODULE\EVA00\pilot_controller.c";
+
         [Fact]
         public void PreprocessMapLinesTest()
         {
@@ -57,6 +74,13 @@ int main() {
         {
             var messages = ExtractMessages(ErrString, PpString);
             Assert.Equal(5, messages.First().Line);
+        }
+
+        [Fact]
+        public void HostSourcePathMappingTest()
+        {
+            var hostSource = HostSourcePathMapping(RemotePath, ProjectPaths);
+            Assert.Equal(@"code/EVA00/pilot_controller.c", hostSource);
         }
     }
 }
