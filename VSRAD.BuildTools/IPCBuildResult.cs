5a8252005b063216351cda563116bdb9e6d02dd0
timlathy <tim.labushev@gmail.com>
2019-11-29 13:08:58 +0300

    begin moving error processing logic away from task dll

--- VSRAD.BuildTools/IPCBuildResult.cs
+++ VSRAD.BuildTools/IPCBuildResult.cs
@@ -7,70 +7,65 @@ namespace VSRAD.BuildTools
 {
     public sealed class IPCBuildResult
     {
-        public const string ServerErrorBuildSkipped = "2057AA22-AFD2-480F-AC16-6912F3077E0D";
+        public enum MessageKind { Error = 0, Warning = 1, Note = 2 }
 
-        public int ExitCode { get; set; }
-        public string Stdout { get; set; }
-        public string Stderr { get; set; }
-        public string ServerError { get; set; }
-        public string PreprocessedSource { get; set; } = "";
-        public string[] ProjectSourcePaths { get; set; } = Array.Empty<string>();
-
-        public static IPCBuildResult Read(Stream stream)
+        public sealed class Message
         {
-            using (var reader = new BinaryReader(stream))
-            {
-                if (reader.ReadBoolean()) // success
-                {
-                    var buildResult = new IPCBuildResult
-                    {
-                        ExitCode = reader.ReadInt32(),
-                        Stdout = reader.ReadString(),
-                        Stderr = reader.ReadString(),
-                        PreprocessedSource = reader.ReadString(),
-                    };
-
-                    var count = reader.ReadUInt32();
-                    buildResult.ProjectSourcePaths = new string[count];
-                    for (var i = 0; i < count; i++)
-                    {
-                        var projectSourcePath = reader.ReadString();
-                        buildResult.ProjectSourcePaths[i] = projectSourcePath;
-                    }
-
-                    return buildResult;
-                }
-                else
-                    return new IPCBuildResult
-                    {
-                        ServerError = reader.ReadString()
-                    };
-            }
+            public MessageKind Kind { get; set; }
+            public string Text { get; set; }
+            public string SourceFile { get; set; }
+            public int Line { get; set; }
+            public int Column { get; set; }
         }
+
+        public bool Skipped { get; set; }
+        public bool Successful { get; set; }
+        public string ServerError { get; set; } = "";
+        public Message[] ErrorMessages { get; set; } = Array.Empty<Message>();
 
         public byte[] ToArray()
         {
             using (var memStream = new MemoryStream())
             using (var writer = new BinaryWriter(memStream))
             {
-                if (ServerError == null)
+                writer.Write(Skipped);
+                writer.Write(Successful);
+                writer.Write(ServerError);
+                writer.Write(ErrorMessages.Length);
+                foreach (var message in ErrorMessages)
                 {
-                    writer.Write(true); // success
-                    writer.Write(ExitCode);
-                    writer.Write(Stdout);
-                    writer.Write(Stderr);
-                    writer.Write(PreprocessedSource);
-                    writer.Write(ProjectSourcePaths.Length);
-                    foreach (var sourcePath in ProjectSourcePaths)
-                        writer.Write(sourcePath);
+                    writer.Write((int)message.Kind);
+                    writer.Write(message.Text);
+                    writer.Write(message.SourceFile);
+                    writer.Write(message.Line);
+                    writer.Write(message.Column);
                 }
-                else
-                {
-                    writer.Write(false); // success
-                    writer.Write(ServerError);
-                }
-
                 return memStream.ToArray();
+            }
+        }
+
+        public static IPCBuildResult Read(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                var buildResult = new IPCBuildResult
+                {
+                    Skipped = reader.ReadBoolean(),
+                    Successful = reader.ReadBoolean(),
+                    ServerError = reader.ReadString()
+                };
+                var messages = new Message[reader.ReadInt32()];
+                for (int m = 0; m < messages.Length; ++m)
+                    messages[m] = new Message
+                    {
+                        Kind = (MessageKind)reader.ReadInt32(),
+                        Text = reader.ReadString(),
+                        SourceFile = reader.ReadString(),
+                        Line = reader.ReadInt32(),
+                        Column = reader.ReadInt32()
+                    };
+                buildResult.ErrorMessages = messages;
+                return buildResult;
             }
         }
 
