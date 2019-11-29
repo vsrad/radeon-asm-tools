5eb68cc22c75d4579df2cd398595747807861443
Slimakanzer <gleb.larochkin@gmail.com>
2019-11-29 17:43:04 +0300

    improved testing data for line mapping

--- VSRAD.BuildToolsTests/Errors/LineMapperTests.cs
+++ VSRAD.BuildToolsTests/Errors/LineMapperTests.cs
@@ -19,17 +19,17 @@ test.c:16:10: error: invalid suffix 'uigyuigyuiguyi' on integer constant
 ";
 
         public const string PpString = @"# 1 ""test.c""
-# 1 ""<built-in>""
+    //# 1 ""<built-in>""
 # 1 ""<command-line>""
 # 31 ""<command-line>""
-# 1 ""/usr/include/stdc-predef.h"" 1 3 4
-# 32 ""<command-line>"" 2
-# 1 ""test.c""
+    # 1     ""/usr/include/stdc-predef.h"" 1 3 4
+//# 32      ""<command-line>"" 2
+#       1 ""test.c""
 
 
 int main(int argc, char** argv)
         {
-# 1 ""code.c"" 1
+  # 1 ""code.c"" 1
             int i = 3 + 3;
             return ia;
 # 5 ""test.c"" 2
