451650ad653a7d642aa350134d210bf2e418228a
johnny-keker <giomail.iv@gmail.com>
2020-01-23 04:16:51 -0800

    fix #38

--- VSRAD.Package/Commands/ToolWindowCommand.cs
+++ VSRAD.Package/Commands/ToolWindowCommand.cs
@@ -1,24 +1,30 @@
-﻿using Microsoft.VisualStudio;
+﻿using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.ProjectSystem;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Collections.Immutable;
+using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 
 namespace VSRAD.Package.Commands
 {
-    [ExportCommandGroup(Constants.VisualizerCommandSet)]
+    [ExportCommandGroup(Constants.ToolWindowCommandSet)]
     [AppliesTo(Constants.ProjectCapability)]
-    internal sealed class DebugVisualizerCommand : IAsyncCommandGroupHandler
+    internal sealed class ToolWindowCommand : IAsyncCommandGroupHandler
     {
+        [Import]
+        internal SVsServiceProvider ServiceProvider = null;
+
         public Task<CommandStatusResult> GetCommandStatusAsync(IImmutableSet<IProjectTree> nodes, long commandId, bool focused, string commandText, CommandStatus progressiveStatus)
         {
             switch (commandId)
             {
-                case Constants.VisualizerWindowCommandId:
-                case Constants.VisualizerOptionsCommandId:
+                case Constants.ToolWindowVisualizerCommandId:
+                case Constants.ToolWindowOptionsCommandId:
+                case Constants.ToolWindowFunctionListCommandId:
                     return Task.FromResult(new CommandStatusResult(true, commandText, CommandStatus.Enabled | CommandStatus.Supported));
                 default:
                     return Task.FromResult(CommandStatusResult.Unhandled);
@@ -28,11 +34,19 @@ namespace VSRAD.Package.Commands
         public async Task<bool> TryHandleCommandAsync(IImmutableSet<IProjectTree> nodes, long commandId, bool focused, long commandExecuteOptions, IntPtr variantArgIn, IntPtr variantArgOut)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (commandId == Constants.ToolWindowFunctionListCommandId)
+            {
+                DTE dte = (DTE)ServiceProvider.GetService(typeof(DTE));
+                dte?.ExecuteCommand("View.FunctionList");
+                return true;
+            }
+
             IVsWindowFrame windowFrame;
 
-            if (commandId == Constants.VisualizerWindowCommandId)
+            if (commandId == Constants.ToolWindowVisualizerCommandId)
                 windowFrame = (IVsWindowFrame)VSPackage.VisualizerToolWindow.Frame;
-            else if (commandId == Constants.VisualizerOptionsCommandId)
+            else if (commandId == Constants.ToolWindowOptionsCommandId)
                 windowFrame = (IVsWindowFrame)VSPackage.OptionsToolWindow.Frame;
             else
                 return false;
--- VSRAD.Package/Constants.cs
+++ VSRAD.Package/Constants.cs
@@ -19,8 +19,9 @@ namespace VSRAD.Package
         public const string ProjectCapability = "RADProject";
 
         public const int MenuCommandId = 0x0100;
-        public const int VisualizerWindowCommandId = 0x0100;
-        public const int VisualizerOptionsCommandId = 0x0101;
+        public const int ToolWindowVisualizerCommandId = 0x0100;
+        public const int ToolWindowOptionsCommandId = 0x0101;
+        public const int ToolWindowFunctionListCommandId = 0x0102;
         public const int ProfileCommandId = 0x0100;
         public const int DisassembleCommandId = 0x0100;
         public const int PreprocessCommandId = 0x0100;
@@ -30,7 +31,7 @@ namespace VSRAD.Package
         public const int AddArrayToWatchesToIdOffset = 0x1400;
         public const int AddArrayToWatchesToFromOffset = 0x100;
         public const int AddArrayToWatchesToHeaderOffset = 0x1200;
-        public const string VisualizerCommandSet = "03c8f3ba-2e44-4159-ac37-b08fc295a0cc";
+        public const string ToolWindowCommandSet = "03c8f3ba-2e44-4159-ac37-b08fc295a0cc";
         public const string ForceRunToCursorCommandSet = "cefc8250-7cd1-46c1-b4f6-46a0a22a1c81";
         public const string AddToWatchesCommandSet = "8560BD12-1D31-40BA-B300-1A31FC901E93";
         public const string AddArrayToWatchesCommandSet = "A03BE90E-E3E1-47F8-815B-387605FDCB73";
