68a3e002e20c748449f520eef044b2dea719659c
johnny-keker <giomail.iv@gmail.com>
2020-02-19 18:12:31 +0300

    reduce number of build warnings

--- VSRAD.Package/Utils/NativeMethods.cs
+++ VSRAD.Package/Utils/NativeMethods.cs
@@ -8,17 +8,17 @@ namespace VSRAD.Package.Utils
     {
         /* https://stackoverflow.com/a/487757 */
         [DllImport("user32.dll")]
-        private static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint wMsg, UIntPtr wParam, IntPtr lParam);
         private const int WM_SETREDRAW = 11;
 
         public static void SuspendDrawing(this Control control)
         {
-            _ = SendMessage(control.Handle, WM_SETREDRAW, false, 0);
+            _ = SendMessage(control.Handle, WM_SETREDRAW, UIntPtr.Zero, IntPtr.Zero);
         }
 
         public static void ResumeDrawing(this Control control)
         {
-            _ = SendMessage(control.Handle, WM_SETREDRAW, true, 0);
+            _ = SendMessage(control.Handle, WM_SETREDRAW, new UIntPtr(1), IntPtr.Zero);
             control.Refresh();
         }
     }
