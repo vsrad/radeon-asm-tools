2cba025dc16dba81de19ae12256a4f5727460089
johnny-keker <giomail.iv@gmail.com>
2020-03-28 18:56:29 +0300

    disabling quick edit feature in windows cmd to prevent server hanging

--- VSRAD.DebugServer/Server.cs
+++ VSRAD.DebugServer/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,6 +13,18 @@ namespace VSRAD.DebugServer
         private readonly TcpListener _listener;
         private readonly bool _verboseLogging;
 
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint lpMode);
+        [DllImport("kernel32.dll")]
+        static extern uint GetLastError();
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
         public Server(IPAddress ip, int port, bool verboseLogging = false)
         {
             _listener = new TcpListener(ip, port);
@@ -20,6 +33,17 @@ namespace VSRAD.DebugServer
 
         public async Task LoopAsync()
         {
+            /* disable Quick Edit cmd feature to prevent server hanging */
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+                UInt32 consoleMode;
+                if (!GetConsoleMode(consoleHandle, out consoleMode))
+                    Console.WriteLine($"Warning! Cannot get console mode. Error code={GetLastError()}");
+                consoleMode &= ~ENABLE_QUICK_EDIT;
+                if (!SetConsoleMode(consoleHandle, consoleMode))
+                    Console.WriteLine($"Warning! Cannot set console mode. Error code={GetLastError()}");
+            }
             _listener.Start();
             uint clientsCount = 0;
             while (true)
