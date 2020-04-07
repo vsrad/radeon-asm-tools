687a05ccb0788023f956e695c295505662f3cfe1
Slimakanzer <gleb.larochkin@gmail.com>
2020-04-07 11:41:13 +0300

    [syntax] cleanup text view observer every 15 opened text view

--- VSRAD.Syntax/Options/TextViewObserver.cs
+++ VSRAD.Syntax/Options/TextViewObserver.cs
@@ -7,13 +7,18 @@ namespace VSRAD.Syntax.Options
     [Export(typeof(TextViewObserver))]
     internal sealed class TextViewObserver
     {
+        private static readonly object lockObj = new object();
         private readonly List<IWpfTextView> _textViews;
+        private int _count;
         public IList<IWpfTextView> TextViews
         {
             get
             {
-                _textViews.RemoveAll(textView => textView.IsClosed);
-                return _textViews;
+                lock (lockObj)
+                {
+                    RemoveUnusedTextViews();
+                    return _textViews;
+                }
             }
         }
 
@@ -21,11 +26,21 @@ namespace VSRAD.Syntax.Options
         public TextViewObserver()
         {
             this._textViews = new List<IWpfTextView>();
+            this._count = 0;
         }
 
         public void WpfTextViewCreated(IWpfTextView wpfTextView)
         {
-            _textViews.Add(wpfTextView);
+            lock (lockObj)
+            {
+                _textViews.Add(wpfTextView);
+                _count++;
+                if (_count >= 15)
+                    RemoveUnusedTextViews();
+            }
         }
+
+        private void RemoveUnusedTextViews() => 
+            _textViews.RemoveAll(textView => textView == null || textView.IsClosed);
     }
 }
