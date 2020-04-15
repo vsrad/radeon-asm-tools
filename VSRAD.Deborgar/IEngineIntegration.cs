54d204e8cd79af89bba58eed6ae1d4cd1170c9b0
timlathy <tim.labushev@gmail.com>
2020-04-15 14:58:15 +0300

    switch betweeen single/multiple breakpoints in debug engine

--- VSRAD.Deborgar/IEngineIntegration.cs
+++ VSRAD.Deborgar/IEngineIntegration.cs
@@ -1,17 +1,20 @@
-﻿using System;
-
-namespace VSRAD.Deborgar
+﻿namespace VSRAD.Deborgar
 {
     public delegate void ExecutionCompleted(bool success);
 
+    public enum BreakMode
+    {
+        SingleRoundRobin, SingleRerun, Multiple
+    }
+
     public interface IEngineIntegration
     {
-        void ExecuteToLine(uint breakLine);
+        void Execute(uint[] breakLines);
         string GetActiveProjectFile();
         string GetProjectRelativePath(string absoluteFilePath);
         uint GetFileLineCount(string projectFilePath);
+        BreakMode GetBreakMode();
         bool PopRunToLineIfSet(string file, out uint runToLine);
-        bool RerunOnContinue();
 
         event ExecutionCompleted ExecutionCompleted;
     }
