33b94272a0b077cc1c7e5d2827db858b489d7227
johnny-keker <giomail.iv@gmail.com>
2020-05-18 18:31:29 +0300

    moved slice visualizer to separate folder, slicer table wip

--- VSRAD.Package/DebugVisualizer/SliceVisualizer/SliceVisualizerControl.xaml.cs
+++ VSRAD.Package/DebugVisualizer/SliceVisualizer/SliceVisualizerControl.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using VSRAD.Package.Options;
 using VSRAD.Package.ToolWindows;
 using VSRAD.Package.Utils;
 
-namespace VSRAD.Package.DebugVisualizer
+namespace VSRAD.Package.DebugVisualizer.SliceVisualizer
 {
     /// <summary>
     /// Interaction logic for SliceVisualizerControl.xaml
     /// </summary>
     public partial class SliceVisualizerControl : UserControl
     {
+
+        private Server.BreakState _breakState;
+
         public sealed class Context : DefaultNotifyPropertyChanged
         {
             public ProjectOptions Options { get; }
@@ -25,7 +39,7 @@ namespace VSRAD.Package.DebugVisualizer
                 Options = options;
             }
         }
-        
+
 
         public SliceVisualizerControl(IToolWindowIntegration integration)
         {
---
+++ VSRAD.Package/DebugVisualizer/SliceVisualizer/SliceVisualizerTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSRAD.Package.DebugVisualizer.SliceVisualizer
+{
+    class SliceVisualizerTable : DataGridView
+    {
+        private const int MaxGroupSize = 512;
+        public SliceVisualizerTable() : base()
+        {
+            for (int i = 0; i < MaxGroupSize; i++)
+            {
+                Columns.Add(new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = i.ToString(),
+                    ReadOnly = true,
+                    SortMode = DataGridViewColumnSortMode.NotSortable
+                });
+            }
+        }
+
+        public void DisplayWatch(List<uint[]> data, uint groupSize)
+        {
+            Rows.Clear();
+
+            for (int i = 1; i <= data.Count; i++)
+            {
+                var index = Rows.Add(new DataGridViewRow());
+                Rows[index].HeaderCell.Value = i;
+            }
+            
+            for (int i = 0; i < groupSize; i++)
+            {
+                for (int j = 0; j < data.Count; j++)
+                    Rows[j].Cells[i].Value = data[j][i];
+            }
+        }
+    }
+}
