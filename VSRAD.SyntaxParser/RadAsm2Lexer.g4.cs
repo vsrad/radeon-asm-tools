8fa02acd0de59c0f1981f29b501a4c48d8f01f73
Slimakanzer <gleb.larochkin@gmail.com>
2020-05-28 16:09:47 +0500

    [Syntax] remove unused source

--- VSRAD.SyntaxParser/RadAsm2Lexer.g4.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace VSRAD.SyntaxParser
-//{
-//    using Antlr4.Runtime;
-//    partial class RadAsm2Lexer : Lexer
-//    {
-//        public RadAsm2Lexer(string input) : this(new AntlrInputStream(input)) { }
-//    }
-//}
