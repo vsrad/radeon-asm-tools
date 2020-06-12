766fb846ff48abac7f2457e5d061ae1dc7e3b38c
timlathy <tim.labushev@gmail.com>
2020-06-12 16:32:43 +0300

    wip action sequence editor

---
+++ VSRAD.Package/Options/Actions.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using VSRAD.Package.Utils;
+
+namespace VSRAD.Package.Options
+{
+    public interface IAction : INotifyPropertyChanged
+    {
+        ActionType Type { get; }
+    }
+
+    public enum ActionType
+    {
+        Local, Remote
+    }
+
+    public enum FileCopyDirection
+    {
+        LocalToRemote, RemoteToLocal
+    }
+
+    public sealed class CopyFileAction : DefaultNotifyPropertyChanged, IAction
+    {
+        public ActionType Type => ActionType.Remote;
+
+        private FileCopyDirection _direction;
+        public FileCopyDirection Direction { get => _direction; set => SetField(ref _direction, value); }
+
+        private string _localPath;
+        public string LocalPath { get => _localPath; set => SetField(ref _localPath, value); }
+
+        private string _remotePath;
+        public string RemotePath { get => _remotePath; set => SetField(ref _remotePath, value); }
+
+        private bool _checkTimestamp;
+        public bool CheckTimestamp { get => _checkTimestamp; set => SetField(ref _checkTimestamp, value); }
+
+        public override string ToString() => "CopyFileAction";
+    }
+
+    public sealed class ExecuteAction : DefaultNotifyPropertyChanged, IAction
+    {
+        private ActionType _type;
+        public ActionType Type { get => _type; set => SetField(ref _type, value); }
+
+        private string _executable;
+        public string Executable { get => _executable; set => SetField(ref _executable, value); }
+
+        private string _arguments;
+        public string Arguments { get => _arguments; set => SetField(ref _arguments, value); }
+    }
+}
---
+++ VSRAD.Package/ProjectSystem/Profiles/ActionSequenceEditor.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+using VSRAD.Package.Options;
+using VSRAD.Package.Utils;
+
+namespace VSRAD.Package.ProjectSystem.Profiles
+{
+    public sealed class ActionSequenceEditorDesignTimeData
+    {
+        public ObservableCollection<IAction> Sequence { get; }
+
+        public ActionSequenceEditorDesignTimeData()
+        {
+            Sequence = new ObservableCollection<IAction>()
+            {
+                new CopyFileAction { Direction = FileCopyDirection.RemoteToLocal, LocalPath = @"C:\Local\Path", RemotePath = "/remote/path", CheckTimestamp = true },
+                new ExecuteAction { Type = ActionType.Remote, Executable = "exe", Arguments = "--args" }
+            };
+        }
+    }
+
+    public partial class ActionSequenceEditor : UserControl
+    {
+        public ICommand MoveUpCommand { get; }
+        public ICommand MoveDownCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand RichEditCommand { get; }
+
+        public ActionSequenceEditor()
+        {
+            DataContext = new ActionSequenceEditorDesignTimeData();
+            MoveUpCommand = new WpfDelegateCommand((i) => MoveAction(i, moveUp: true));
+            MoveDownCommand = new WpfDelegateCommand((i) => MoveAction(i, moveUp: false));
+            DeleteCommand = new WpfDelegateCommand(DeleteAction);
+            RichEditCommand = new WpfDelegateCommand(OpenMacroEditor);
+            InitializeComponent();
+        }
+
+        private void MoveAction(object item, bool moveUp)
+        {
+            var sequence = ((ActionSequenceEditorDesignTimeData)DataContext).Sequence;
+            var index = sequence.IndexOf((IAction)item);
+            Debug.Assert(index >= 0);
+            if (moveUp && index > 0)
+                sequence.Move(index, index - 1);
+            else if (!moveUp && index != sequence.Count - 1)
+                sequence.Move(index, index + 1);
+        }
+
+        private void DeleteAction(object item)
+        {
+            Debug.Assert(item != null);
+            ((ActionSequenceEditorDesignTimeData)DataContext).Sequence.Remove((IAction)item);
+        }
+
+        private void OpenMacroEditor(object sender)
+        {
+            var editButton = (Button)sender;
+            var action = editButton.DataContext;
+            var propertyName = (string)editButton.Tag;
+            var property = action.GetType().GetProperty(propertyName);
+
+            // TODO: invoke macro editor
+            var newValue = "(Edited " + (string)property.GetValue(action) + ")";
+            property.SetValue(action, newValue);
+        }
+    }
+}
