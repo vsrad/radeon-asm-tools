125e6bbe2091126728c040043dd2f3ed618f3ea7
timlathy <tim.labushev@gmail.com>
2020-07-11 10:44:31 +0300

    implement put file command (local->remote copy step)

---
+++ VSRAD.DebugServer/Handlers/PutFileHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using VSRAD.DebugServer.IPC.Commands;
+using VSRAD.DebugServer.IPC.Responses;
+
+namespace VSRAD.DebugServer.Handlers
+{
+    public sealed class PutFileHandler : IHandler
+    {
+        private readonly PutFileCommand _command;
+
+        public PutFileHandler(PutFileCommand command)
+        {
+            _command = command;
+        }
+
+        public Task<IResponse> RunAsync()
+        {
+            var fullPath = Path.Combine(_command.WorkDir, _command.Path);
+            var status = DoPutFile(fullPath, _command.Data);
+            return Task.FromResult<IResponse>(new PutFileResponse { Status = status });
+        }
+
+        private static PutFileStatus DoPutFile(string fullPath, byte[] data)
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+                File.WriteAllBytes(fullPath, data);
+                return PutFileStatus.Successful;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return PutFileStatus.PermissionDenied;
+            }
+            catch (IOException)
+            {
+                return PutFileStatus.OtherIOError;
+            }
+        }
+    }
+}
---
+++ VSRAD.DebugServerTests/Handlers/PutFileHandlerTest.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using VSRAD.DebugServer.IPC.Commands;
+using VSRAD.DebugServer.IPC.Responses;
+using Xunit;
+
+namespace VSRAD.DebugServerTests.Handlers
+{
+    public class PutFileHandlerTest
+    {
+        [Fact]
+        public async Task SuccessTestAsync()
+        {
+            var tmpPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            var response = await Helper.DispatchCommandAsync<PutFileCommand, PutFileResponse>(new PutFileCommand
+            {
+                Data = Encoding.UTF8.GetBytes("putfilehandlertest"),
+                Path = Path.GetFileName(tmpPath),
+                WorkDir = Path.GetDirectoryName(tmpPath)
+            });
+
+            Assert.Equal(PutFileStatus.Successful, response.Status);
+            Assert.Equal("putfilehandlertest", File.ReadAllText(tmpPath));
+            File.Delete(tmpPath);
+        }
+
+        [Fact]
+        public async Task PermissionDeniedTestAsync()
+        {
+            var tmpPath = Path.GetTempFileName();
+            File.SetAttributes(tmpPath, FileAttributes.ReadOnly);
+
+            var response = await Helper.DispatchCommandAsync<PutFileCommand, PutFileResponse>(new PutFileCommand
+            {
+                Data = Encoding.UTF8.GetBytes("putfilehandlertest"),
+                Path = Path.GetFileName(tmpPath),
+                WorkDir = Path.GetDirectoryName(tmpPath)
+            });
+
+            Assert.Equal(PutFileStatus.PermissionDenied, response.Status);
+            File.SetAttributes(tmpPath, FileAttributes.Normal);
+            File.Delete(tmpPath);
+        }
+
+        [Fact]
+        public async Task DirectoryNotFoundTestAsync()
+        {
+            var relFile = Path.GetRandomFileName();
+            var relDir = Path.GetRandomFileName();
+            var absDir = Path.Combine(Path.GetTempPath(), relDir);
+
+            Assert.False(Directory.Exists(absDir));
+
+            var response = await Helper.DispatchCommandAsync<PutFileCommand, PutFileResponse>(new PutFileCommand
+            {
+                Data = Encoding.UTF8.GetBytes("putfilehandlertest"),
+                Path = Path.Combine(relDir, relFile),
+                WorkDir = Path.GetTempPath()
+            });
+
+            Assert.Equal(PutFileStatus.Successful, response.Status);
+            Assert.True(Directory.Exists(absDir));
+            Assert.Equal("putfilehandlertest", File.ReadAllText(Path.Combine(absDir, relFile)));
+            Directory.Delete(absDir, recursive: true);
+        }
+
+        [Fact]
+        public async Task IOErrorTestAsync()
+        {
+            var tmpPath = Path.GetTempFileName();
+
+            using (var fs = new FileStream(tmpPath, FileMode.Open))
+            {
+                var response = await Helper.DispatchCommandAsync<PutFileCommand, PutFileResponse>(new PutFileCommand
+                {
+                    Data = Encoding.UTF8.GetBytes("putfilehandlertest"),
+                    Path = tmpPath,
+                    WorkDir = Path.GetTempPath()
+                });
+                Assert.Equal(PutFileStatus.OtherIOError, response.Status);
+            }
+
+            File.Delete(tmpPath);
+        }
+    }
+}
