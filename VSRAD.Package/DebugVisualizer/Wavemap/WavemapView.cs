0adb06f663f98575ae84072e2511fd906603cefc
johnny-keker <giomail.iv@gmail.com>
2020-07-14 16:37:44 +0300

    wavemap view active group and group executed test

---
+++ VSRAD.Package/DebugVisualizer/Wavemap/WavemapView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSRAD.Package.DebugVisualizer.Wavemap
+{
+    public sealed class WavemapView
+    {
+        private readonly int _groupSize;
+        private readonly int _laneDataSize;
+        private readonly int _groupCount;
+
+        private readonly uint[] _data;
+
+        public WavemapView(uint[] data, int groupSize, int laneDataSize, int groupCount)
+        {
+            _data = data;
+            _groupSize = groupSize;
+            _groupCount = groupCount;
+            _laneDataSize = laneDataSize;
+        }
+
+        public bool IsActiveGroup(int groupIndex) => groupIndex < _groupCount;
+
+        public bool GroupExecuted(int groupIndex)
+        {
+            var execMaskIndex = groupIndex * _groupSize * _laneDataSize + (8 * _laneDataSize); // execmask is in 8-9 lane of system watch
+            return _data[execMaskIndex] != 0 || _data[execMaskIndex + _laneDataSize] != 0;
+        }
+    }
+}
---
+++ VSRAD.PackageTests/DebugVisualizer/WavemapTests.cs
@@ -0,0 +1,37 @@
+﻿using VSRAD.Package.DebugVisualizer.Wavemap;
+using Xunit;
+
+namespace VSRAD.PackageTests.DebugVisualizer
+{
+    public class WavemapTests
+    {
+        // 4 groups, 1 watch + system, group size 11
+        private readonly uint[] _data = new uint[]
+        {
+            777, 0, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 100, 0, 100, 0, 0, 0,  // 1-st group, break on line 15, not-empty exec-mask
+            777, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1,       // 2-nd group, empty exec-mask
+            777, 0, 105, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 100, 0, 0, 0, // 3-rd group, break on line 105, not-empty exec-mask
+            777, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1,       // 4-th group, empty exec-mask TODO: fix
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // extra data
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // extra data
+        };
+
+        [Fact]
+        public void IsActiveGroupTest()
+        {
+            var wavemapView = new WavemapView(_data, groupSize: 11, laneDataSize: 2, groupCount: 4);
+            for (int i = 0; i < 10; ++i)
+                Assert.Equal(i < 4, wavemapView.IsActiveGroup(i));
+        }
+
+        [Fact]
+        public void GroupExecutedTest()
+        {
+            var wavemapView = new WavemapView(_data, groupSize: 11, laneDataSize: 2, groupCount: 4);
+
+            var executionMap = new bool[] { true, false, true, false };
+            for (int i = 0; i < 4; ++i)
+                Assert.Equal(executionMap[i], wavemapView.GroupExecuted(i));
+        }
+    }
+}
