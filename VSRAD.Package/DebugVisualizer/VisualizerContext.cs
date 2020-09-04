9d58e15a9bf9452467478ec27ed16c1fb962cef8
johnny-keker <giomail.iv@gmail.com>
2020-09-04 16:21:07 +0300

    refactor

--- VSRAD.Package/DebugVisualizer/VisualizerContext.cs
+++ VSRAD.Package/DebugVisualizer/VisualizerContext.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 using VSRAD.Package.ProjectSystem;
 using VSRAD.Package.Server;
 using VSRAD.Package.Utils;
@@ -60,22 +58,27 @@ namespace VSRAD.Package.DebugVisualizer
         private void EnterBreak(BreakState breakState)
         {
             _breakState = breakState;
-            UpdateProjectState(breakState);
             WatchesValid = breakState != null;
             if (WatchesValid)
+            {
+                UpdateProjectState(breakState);
                 GroupIndex.Update();
+            }
         }
 
         private void UpdateProjectState(BreakState breakState)
         {
-            if (breakState == null) return;
+            Options.VisualizerOptions.NDRange3D = breakState.NDRange3D;
 
-            Options.VisualizerOptions.NDRange3D = breakState.GridY != 0 && breakState.GridZ != 0;
-            Options.DebuggerOptions.GroupSize = breakState.GroupX;
-            GroupIndex.DimX = breakState.GridX;
-            GroupIndex.DimY = breakState.GridY;
-            GroupIndex.DimZ = breakState.GridZ;
-            // TODO: handle 3d group sizes and wavesize
+            Options.DebuggerOptions.GroupSize = breakState.DimX;
+            GroupIndex.DimX = breakState.DimX;
+            GroupIndex.DimY = breakState.DimY;
+            GroupIndex.DimZ = breakState.DimZ;
+
+            Options.DebuggerOptions.NGroups = breakState.NDRange3D
+                ? breakState.DimX * breakState.DimY * breakState.DimZ
+                : breakState.DimX;
+            // TODO: handle wavesize
         }
 
         private void GroupIndexChanged(object sender, GroupIndexChangedEventArgs e)
