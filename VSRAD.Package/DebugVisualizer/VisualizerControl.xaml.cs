3852eaba5031c2fa5040f27b005069e63bd8878c
johnny-keker <giomail.iv@gmail.com>
2020-09-26 14:03:51 +0300

    added wavemap canvas class

--- VSRAD.Package/DebugVisualizer/VisualizerControl.xaml.cs
+++ VSRAD.Package/DebugVisualizer/VisualizerControl.xaml.cs
@@ -22,7 +22,7 @@ namespace VSRAD.Package.DebugVisualizer
             DataContext = _context;
             InitializeComponent();
 
-            SetupWavemapSample();
+            _ = new WavemapCanvas(HeaderHost.WavemapCanvas);
 
             integration.AddWatch += AddWatch;
             integration.ProjectOptions.VisualizerOptions.PropertyChanged += OptionsChanged;
@@ -47,25 +47,6 @@ namespace VSRAD.Package.DebugVisualizer
             _table.SetScalingMode(_context.Options.VisualizerAppearance.ScalingMode);
             TableHost.Setup(_table);
             RestoreSavedState();
-        }
-
-        private void SetupWavemapSample()
-        {
-            for (int y = 0; y < 2; ++y)
-            {
-                for (int i = 0, x = 1; i < 200; ++i, x += 6)
-                {
-                    var r = new Rectangle();
-                    r.Fill = Brushes.Gray;
-                    r.Height = 7;
-                    r.Width = 7;
-                    r.StrokeThickness = 1;
-                    r.Stroke = Brushes.Black;
-                    Canvas.SetLeft(r, x);
-                    Canvas.SetTop(r, 7 * y);
-                    HeaderHost.WavemapCanvas.Children.Add(r);
-                }
-            }
         }
 
         private void DebuggerOptionChanged(object sender, PropertyChangedEventArgs e)
---
+++ VSRAD.Package/DebugVisualizer/WavemapCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace VSRAD.Package.DebugVisualizer
+{
+    class WavemapCanvas
+    {
+        private readonly Canvas _canvas;
+        private readonly Rectangle[][] _rectangles = { new Rectangle[200], new Rectangle[200] };
+
+        public WavemapCanvas(Canvas canvas)
+        {
+            _canvas = canvas;
+            for (int y = 0; y < 2; ++y)
+            {
+                for (int i = 0, x = 1; i < 200; ++i, x += 6)
+                {
+                    var r = new Rectangle();
+                    r.Fill = Brushes.Gray;
+                    r.Height = 7;
+                    r.Width = 7;
+                    r.StrokeThickness = 1;
+                    r.Stroke = Brushes.Black;
+                    Canvas.SetLeft(r, x);
+                    Canvas.SetTop(r, 7 * y);
+                    _rectangles[y][i] = r;
+                    _canvas.Children.Add(r);
+                }
+            }
+        }
+    }
+}
