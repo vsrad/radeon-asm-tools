1151053e6f9bbd95cf607a71aabe3cec57bfedba
johnny-keker <giomail.iv@gmail.com>
2020-09-26 13:17:42 +0300

    sample wavemap interface

--- VSRAD.Package/DebugVisualizer/VisualizerControl.xaml.cs
+++ VSRAD.Package/DebugVisualizer/VisualizerControl.xaml.cs
@@ -22,13 +22,7 @@ namespace VSRAD.Package.DebugVisualizer
             DataContext = _context;
             InitializeComponent();
 
-            var r = new Rectangle();
-            r.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-            r.Height = 20;
-            r.Width = 20;
-            Canvas.SetLeft(r, 0);
-            Canvas.SetTop(r, 0);
-            HeaderHost.WavemapCanvas.Children.Add(r);
+            SetupWavemapSample();
 
             integration.AddWatch += AddWatch;
             integration.ProjectOptions.VisualizerOptions.PropertyChanged += OptionsChanged;
@@ -53,6 +47,25 @@ namespace VSRAD.Package.DebugVisualizer
             _table.SetScalingMode(_context.Options.VisualizerAppearance.ScalingMode);
             TableHost.Setup(_table);
             RestoreSavedState();
+        }
+
+        private void SetupWavemapSample()
+        {
+            for (int y = 0; y < 2; ++y)
+            {
+                for (int i = 0, x = 1; i < 200; ++i, x += 6)
+                {
+                    var r = new Rectangle();
+                    r.Fill = Brushes.Gray;
+                    r.Height = 7;
+                    r.Width = 7;
+                    r.StrokeThickness = 1;
+                    r.Stroke = Brushes.Black;
+                    Canvas.SetLeft(r, x);
+                    Canvas.SetTop(r, 7 * y);
+                    HeaderHost.WavemapCanvas.Children.Add(r);
+                }
+            }
         }
 
         private void DebuggerOptionChanged(object sender, PropertyChangedEventArgs e)
