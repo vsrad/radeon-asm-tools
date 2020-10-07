85c861ab4d5c8080893cad1d552e2ae682b277b4
timlathy <tim.labushev@gmail.com>
2020-10-07 15:50:27 +0300

    add integration tests for status file handling

--- VSRAD.PackageTests/Server/DebugSessionTests.cs
+++ VSRAD.PackageTests/Server/DebugSessionTests.cs
@@ -48,6 +48,7 @@ namespace VSRAD.PackageTests.Server
             Assert.Collection(breakState.Data.Watches,
                 (first) => Assert.Equal("jill", first),
                 (second) => Assert.Equal("julianne", second));
+            Assert.Null(breakState.DispatchParameters);
         }
 
         [Fact]
@@ -87,6 +88,66 @@ namespace VSRAD.PackageTests.Server
 
             result = await session.ExecuteAsync(new[] { 13u }, null);
             Assert.Equal("Local debugger output paths are not supported in this version of RAD Debugger.", result.Error.Value.Message);
+        }
+
+        [Fact]
+        public async Task SuccessfulRunWithStatusFileTestAsync()
+        {
+            var channel = new MockCommunicationChannel();
+            var project = TestHelper.MakeProjectWithProfile(remoteWorkDir: "/remote/workdir").Object;
+            project.Options.SetProfiles(new Dictionary<string, ProfileOptions> { { "Default", new ProfileOptions() } }, activeProfile: "Default");
+            project.Options.Profile.Debugger.Steps.Add(new ExecuteStep { Executable = "va11" });
+            project.Options.Profile.Debugger.OutputFile.Path = "/glitch/city";
+            project.Options.Profile.Debugger.StatusFile.Path = "/glitch/city/status";
+
+            channel.ThenRespond(new[] { new MetadataFetched { Status = FetchStatus.FileNotFound }, new MetadataFetched { Status = FetchStatus.FileNotFound } }); // init timestamp fetch
+            channel.ThenRespond(new ExecutionCompleted { Status = ExecutionStatus.Completed, ExitCode = 0 });
+            channel.ThenRespond(new IResponse[]
+            {
+                // status
+                new ResultRangeFetched { Status = FetchStatus.Successful, Data = Encoding.UTF8.GetBytes(@"
+grid size (8192, 0, 0)
+group size (512, 0, 0)
+wave size 32
+comment 115200"), Timestamp = DateTime.Now },
+                new MetadataFetched { Status = FetchStatus.Successful, Timestamp = DateTime.Now } // output
+            });
+
+            var session = new DebugSession(project, channel.Object, new Mock<IFileSynchronizationManager>().Object, null);
+            var result = await session.ExecuteAsync(new[] { 13u }, new ReadOnlyCollection<string>(new[] { "watch" }.ToList()));
+            Assert.True(channel.AllInteractionsHandled);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.BreakState.DispatchParameters);
+            Assert.Equal<uint>(8192 / 512, result.BreakState.DispatchParameters.DimX);
+            Assert.Equal<uint>(512, result.BreakState.DispatchParameters.GroupSize);
+            Assert.Equal<uint>(32, result.BreakState.DispatchParameters.WaveSize);
+            Assert.False(result.BreakState.DispatchParameters.NDRange3D);
+            Assert.Equal("115200", result.BreakState.DispatchParameters.StatusString);
+        }
+
+        [Fact]
+        public async Task EmptyStatusFileTestAsync()
+        {
+            var channel = new MockCommunicationChannel();
+            var project = TestHelper.MakeProjectWithProfile(remoteWorkDir: "/remote/workdir").Object;
+            project.Options.SetProfiles(new Dictionary<string, ProfileOptions> { { "Default", new ProfileOptions() } }, activeProfile: "Default");
+            project.Options.Profile.Debugger.Steps.Add(new ExecuteStep { Executable = "va11" });
+            project.Options.Profile.Debugger.OutputFile.Path = "/glitch/city";
+            project.Options.Profile.Debugger.StatusFile.Path = "/glitch/city/status";
+
+            channel.ThenRespond(new[] { new MetadataFetched { Status = FetchStatus.FileNotFound }, new MetadataFetched { Status = FetchStatus.FileNotFound } }); // init timestamp fetch
+            channel.ThenRespond(new ExecutionCompleted { Status = ExecutionStatus.Completed, ExitCode = 0 });
+            channel.ThenRespond(new IResponse[]
+            {
+                new ResultRangeFetched { Status = FetchStatus.Successful, Data = Array.Empty<byte>(), Timestamp = DateTime.Now }, // status
+                new MetadataFetched { Status = FetchStatus.Successful, Timestamp = DateTime.Now } // output
+            });
+
+            var session = new DebugSession(project, channel.Object, new Mock<IFileSynchronizationManager>().Object, null);
+            var result = await session.ExecuteAsync(new[] { 13u }, new ReadOnlyCollection<string>(new[] { "watch" }.ToList()));
+            Assert.True(channel.AllInteractionsHandled);
+            Assert.Null(result.BreakState);
+            Assert.Equal("Could not set dispatch parameters from the status file. Make sure that the status file contents match the format.", result.Error.Value.Message);
         }
     }
 }
