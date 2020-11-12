380d70c28f6cc53fc8ee6a60824230c6335553b8
johnny-keker <giomail.iv@gmail.com>
2020-11-12 14:41:24 +0300

    error showing refactor: removed extra boxing

--- VSRAD.Package/Commands/ActionsMenuCommand.cs
+++ VSRAD.Package/Commands/ActionsMenuCommand.cs
@@ -111,8 +111,8 @@ namespace VSRAD.Package.Commands
         {
             if (_currentActionName != null)
             {
-                Errors.Show(new Error($"Action {_currentActionName} is already running.\nIf you believe this to be a hang, use the Disconnect button available in " +
-                    $"Tools -> RAD Debug -> Options to abort the currently running action."));
+                Errors.ShowWarning($"Action {_currentActionName} is already running.\nIf you believe this to be a hang, use the Disconnect button available in " +
+                    $"Tools -> RAD Debug -> Options to abort the currently running action.");
                 return;
             }
 
