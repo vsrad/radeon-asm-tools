f97b79c7598839115c6e80dabb99eac60762b171
johnny-keker <giomail.iv@gmail.com>
2021-03-09 19:38:14 +0300

    custom combo binding works

--- VSRAD.Package/Utils/RecentlyUsedCollection.cs
+++ VSRAD.Package/Utils/RecentlyUsedCollection.cs
@@ -7,19 +7,36 @@ using System.Threading.Tasks;
 
 namespace VSRAD.Package.Utils
 {
-    public sealed class RecentlyUsedCollection : ObservableCollection<string>
+    public sealed class LastUsed : DefaultNotifyPropertyChanged
+    {
+        private string _value;
+        public string Value { get => _value; set => SetField(ref _value, value); }
+
+        private bool _pinned;
+        public bool Pinned { get => _pinned; set => SetField(ref _pinned, value); }
+
+        public LastUsed(string value)
+        {
+            Value = value;
+            Pinned = false;
+        }
+    }
+
+    public sealed class RecentlyUsedCollection : ObservableCollection<LastUsed>
     {
         private const int MAX_COUNT = 10;
 
         public void AddElement(string value)
         {
             if (string.IsNullOrWhiteSpace(value)) return;
-            if (IndexOf(value) is var oldIndex && oldIndex != -1)
+
+            var newElement = new LastUsed(value);
+            if (IndexOf(newElement) is var oldIndex && oldIndex != -1)
             {
                 Move(oldIndex, 0);
                 return;
             }
-            Insert(0, value);
+            Insert(0, newElement);
             if (Count > MAX_COUNT) RemoveAt(MAX_COUNT);
         }
 
