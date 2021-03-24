56a055190ae4b54a756bebe984e05f431461cbf8
timlathy <tim.labushev@gmail.com>
2021-03-24 17:47:21 +0300

    implement get files command

--- VSRAD.DebugServer/Dispatcher.cs
+++ VSRAD.DebugServer/Dispatcher.cs
@@ -16,6 +16,7 @@ namespace VSRAD.DebugServer
             PutFileCommand pf => new PutFileHandler(pf).RunAsync(),
             PutDirectoryCommand pd => new PutDirectoryHandler(pd).RunAsync(),
             ListFilesCommand lf => new ListFilesHandler(lf).RunAsync(),
+            GetFilesCommand gf => new GetFilesHandler(gf).RunAsync(),
             Deploy d => new DeployHandler(d, clientLog).RunAsync(),
             ListEnvironmentVariables lev => new ListEnvironmentVariablesHandler(lev).RunAsync(),
             _ => throw new ArgumentException($"Unknown command type {command.GetType()}"),
