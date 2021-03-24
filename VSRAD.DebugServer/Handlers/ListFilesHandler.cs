a72f016a394379f5c64ea69c3760d162f8f4997b
timlathy <tim.labushev@gmail.com>
2021-03-24 13:22:08 +0300

    share file listing code between debug server and vsix

--- VSRAD.DebugServer/Handlers/ListFilesHandler.cs
+++ VSRAD.DebugServer/Handlers/ListFilesHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using VSRAD.DebugServer.IPC.Commands;
 using VSRAD.DebugServer.IPC.Responses;
@@ -19,29 +17,7 @@ namespace VSRAD.DebugServer.Handlers
         public Task<IResponse> RunAsync()
         {
             var path = Path.Combine(_command.WorkDir, _command.Path);
-
-            var files = new List<(string RelativePath, bool IsDirectory, long Size, DateTime Timestamp)>();
-
-            if (Directory.Exists(path))
-            {
-                files.Add((".", true, 0, File.GetLastWriteTimeUtc(path)));
-
-                var root = new DirectoryInfo(path);
-                foreach (var entry in root.EnumerateFileSystemInfos("*", SearchOption.AllDirectories))
-                {
-                    var relPath = entry.FullName.Substring(root.FullName.Length + 1);
-                    if (entry is FileInfo file)
-                        files.Add((relPath, false, file.Length, file.LastWriteTimeUtc));
-                    else
-                        files.Add((relPath, true, 0, entry.LastWriteTimeUtc));
-                }
-            }
-            else if (File.Exists(path))
-            {
-                var file = new FileInfo(path);
-                files.Add((".", false, file.Length, file.LastWriteTimeUtc));
-            }
-
+            var files = FileMetadata.GetMetadataForTree(path);
             return Task.FromResult<IResponse>(new ListFilesResponse { Files = files.ToArray() });
         }
     }
