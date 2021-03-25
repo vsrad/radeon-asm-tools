613a1b3fd5c8802a238d609ef323d1edcd17646a
timlathy <tim.labushev@gmail.com>
2021-03-25 16:40:45 +0300

    add an option to exclude subdirectories in copy file step

--- VSRAD.DebugServer/SharedUtils/FileMetadata.cs
+++ VSRAD.DebugServer/SharedUtils/FileMetadata.cs
@@ -6,41 +6,45 @@ namespace VSRAD.DebugServer.SharedUtils
 {
     public readonly struct FileMetadata : IEquatable<FileMetadata>
     {
+        /// <summary>
+        /// Paths use / as the directory separator. Paths ending with the slash are interpreted as directories:
+        /// this matches the behavior of zip archives used in <see cref="IPC.Commands.GetFilesCommand"/> and <see cref="IPC.Commands.PutDirectoryCommand"/>.
+        /// </summary>
         public string RelativePath { get; }
-        public bool IsDirectory { get; }
+        public bool IsDirectory => RelativePath.EndsWith("/");
         public long Size { get; }
         public DateTime LastWriteTimeUtc { get; }
 
-        public FileMetadata(string relativePath, bool isDirectory, long size, DateTime lastWriteTimeUtc)
+        public FileMetadata(string relativePath, long size, DateTime lastWriteTimeUtc)
         {
             RelativePath = relativePath;
-            IsDirectory = isDirectory;
             Size = size;
             LastWriteTimeUtc = lastWriteTimeUtc;
         }
 
-        public static List<FileMetadata> GetMetadataForTree(string path)
+        public static List<FileMetadata> GetMetadataForPath(string path, bool recursive)
         {
             var files = new List<FileMetadata>();
 
             if (Directory.Exists(path))
             {
-                files.Add(new FileMetadata(".", isDirectory: true, 0, File.GetLastWriteTimeUtc(path)));
+                files.Add(new FileMetadata("./", 0, File.GetLastWriteTimeUtc(path)));
 
                 var root = new DirectoryInfo(path);
-                foreach (var entry in root.EnumerateFileSystemInfos("*", SearchOption.AllDirectories))
+                var searchOpts = recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                foreach (var entry in root.EnumerateFileSystemInfos("*", searchOpts))
                 {
-                    var relPath = entry.FullName.Substring(root.FullName.Length + 1);
+                    var relPath = entry.FullName.Substring(root.FullName.Length + 1).Replace('\\', '/');
                     if (entry is FileInfo file)
-                        files.Add(new FileMetadata(relPath, isDirectory: false, file.Length, file.LastWriteTimeUtc));
-                    else
-                        files.Add(new FileMetadata(relPath, isDirectory: true, 0, entry.LastWriteTimeUtc));
+                        files.Add(new FileMetadata(relPath, file.Length, file.LastWriteTimeUtc));
+                    else if (recursive)
+                        files.Add(new FileMetadata(relPath + '/', 0, entry.LastWriteTimeUtc));
                 }
             }
             else if (File.Exists(path))
             {
                 var file = new FileInfo(path);
-                files.Add(new FileMetadata(".", isDirectory: false, file.Length, file.LastWriteTimeUtc));
+                files.Add(new FileMetadata(".", file.Length, file.LastWriteTimeUtc));
             }
 
             return files;
