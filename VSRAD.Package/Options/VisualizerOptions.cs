1d684e565644acbe087b0ae35ebe05c76bb1d1fe
johnny-keker <giomail.iv@gmail.com>
2021-05-07 15:59:47 +0300

    renamed break args to app args 2, added app args 3

--- VSRAD.Package/Options/VisualizerOptions.cs
+++ VSRAD.Package/Options/VisualizerOptions.cs
@@ -41,9 +41,13 @@ namespace VSRAD.Package.Options
         [DefaultValue(true)]
         public bool ShowAppArgsField { get => _showAppArgsField; set => SetField(ref _showAppArgsField, value); }
 
-        private bool _showBreakArgsField;
+        private bool _showAppArgs2Field;
         [DefaultValue(true)]
-        public bool ShowBreakArgsField { get => _showBreakArgsField; set => SetField(ref _showBreakArgsField, value); }
+        public bool ShowAppArgs2Field { get => _showAppArgs2Field; set => SetField(ref _showAppArgs2Field, value); }
+
+        private bool _showAppArgs3Field;
+        [DefaultValue(true)]
+        public bool ShowAppArgs3Field { get => _showAppArgs3Field; set => SetField(ref _showAppArgs3Field, value); }
 
         private bool _showWavemap;
         [DefaultValue(true)]
