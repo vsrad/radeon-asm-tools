eacd976726f368b6cb2d1767bc19b1ab5d912af0
Slimakanzer <glarochk@amd.com>
2021-06-19 15:17:01 +0300

    update debug scripts

--- Example/VectorAddProjectExample/common/debugger/vadd_debug.sh
+++ Example/VectorAddProjectExample/common/debugger/vadd_debug.sh
@@ -1,44 +1,46 @@
 #!/bin/bash -x
 
+USAGE="Usage: $0 -l line -f source_file_path -o debug_buffer_path -w watches -t counter [-a app_args] [-p perl_args]"
+
 while getopts "l:f:o:w:t:a:p:" opt
 do
 	echo "$opt $OPTARG"
 	case "$opt" in
-	l) line=$OPTARG
-	;;
-	f) src_path=$OPTARG
-	;;
-	o) dump_path=$OPTARG
-	;;
-	w) watches=$OPTARG
-	;;
-	t) counter=$OPTARG
-	;;
-	a) app_args=$OPTARG
-	;;
-	p) perl_args=$OPTARG
-	;;
+	l) line=$OPTARG ;;
+	f) src_path=$OPTARG ;;
+	o) buffer_path=$OPTARG ;;
+	w) watches=$OPTARG ;;
+	t) counter=$OPTARG ;;
+	a) app_args=$OPTARG ;;
+	p) perl_args=$OPTARG ;;
 	esac
 done
 
-rm -rf tmp_dir
-mkdir tmp_dir
-tmp=tmp_dir/tmp_gcn_breakpoint_pl.s
+[[ -z "$line" || -z "$src_path" || -z "$buffer_path" || -z "$watches" ]] && { echo $USAGE; exit 1; }
 
-num_watches=`echo "${watches}" | awk -F":" '{print NF}'`
+rm -rf tmp
+mkdir tmp
+export TMPPATH="tmp/"
 
-export BREAKPOINT_SCRIPT_OPTIONS="-l $line -o $tmp -t $counter $perl_args"
+SCRIPTPATH=`dirname $0`
+export VADDPATH=`realpath $SCRIPTPATH/../../`
+
+export BREAKPOINT_SCRIPT_OPTIONS="-l $line -t $counter $perl_args"
 export BREAKPOINT_SCRIPT_WATCHES="$watches"
-export ASM_DBG_BUF_SIZE=4194304
-export VADD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../../"
+export DBG_BUF_SIZE=4194304
 
-"$VADD_DIR/build/gfx9/fp32_v_add" \
-	-asm "$VADD_DIR/common/debugger/dbg_clang_wrapper.sh" \
-	-s "$src_path" \
-	-I "$VADD_DIR/gfx9/include" \
-	-o "./tmp_dir/fp32_v_add.co" \
-	-b "$dump_path" \
-	-bsz "$ASM_DBG_BUF_SIZE" \
+GFX=`/opt/rocm/bin/rocminfo | grep -om1 gfx9..`
+CLANG="$VADDPATH/common/debugger/dbg_clang_wrapper.sh"
+CLANG_ARGS="-x assembler -target amdgcn--amdhsa -mcpu=$GFX -I$VADDPATH/gfx9/include"
+
+CO_PATH="$TMPPATH/fp32_v_add.co"
+ASM_CMD="cat $src_path | $CLANG $CLANG_ARGS -o $CO_PATH"
+
+"$VADDPATH/build/gfx9/fp32_v_add" \
+	-asm "$ASM_CMD"               \
+	-c 	 "$CO_PATH"               \
+	-b   "$buffer_path"           \
+	-bsz "$ASM_DBG_BUF_SIZE"      \
 	$app_args
 
 echo
