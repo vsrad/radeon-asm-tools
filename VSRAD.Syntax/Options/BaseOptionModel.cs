5263e131dc71e918d9db02547ff72aec4d561688
slimakanzer <glarochk@amd.com>
2021-07-20 16:29:13 +0300

    [syntax] refactor options

---
+++ VSRAD.Syntax/Options/BaseOptionModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using Microsoft;
+using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell.Settings;
+using Microsoft.VisualStudio.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSRAD.Syntax.Options
+{
+    public abstract class BaseOptionModel<T> where T : BaseOptionModel<T>, new()
+    {
+        private static readonly AsyncLazy<T> _liveModel = new AsyncLazy<T>(CreateAsync, ThreadHelper.JoinableTaskFactory);
+        protected static readonly AsyncLazy<ShellSettingsManager> _settingsManager = new AsyncLazy<ShellSettingsManager>(GetSettingsManagerAsync, ThreadHelper.JoinableTaskFactory);
+
+        protected BaseOptionModel()
+        { }
+
+        public static T Instance
+        {
+            get
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+                return ThreadHelper.JoinableTaskFactory.Run(GetInstanceAsync);
+            }
+        }
+
+        public static Task<T> GetInstanceAsync() => _liveModel.GetValueAsync();
+
+        public static async Task<T> CreateAsync()
+        {
+            var instance = new T();
+            await instance.LoadAsync();
+            return instance;
+        }
+
+        protected virtual string CollectionName { get; } = typeof(T).FullName;
+
+        public virtual void Load()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(LoadAsync);
+        }
+
+        public virtual async Task LoadAsync()
+        {
+            var manager = await _settingsManager.GetValueAsync();
+            var settingsStore = manager.GetReadOnlySettingsStore(SettingsScope.UserSettings);
+
+            if (!settingsStore.CollectionExists(CollectionName))
+            {
+                return;
+            }
+
+            foreach (PropertyInfo property in GetOptionProperties())
+            {
+                try
+                {
+                    string serializedProp = settingsStore.GetString(CollectionName, property.Name);
+                    object value = DeserializeValue(serializedProp, property.PropertyType);
+                    property.SetValue(this, value);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Write(ex);
+                }
+            }
+        }
+
+        public virtual void Save()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(SaveAsync);
+        }
+
+        public virtual async Task SaveAsync()
+        {
+            ShellSettingsManager manager = await _settingsManager.GetValueAsync();
+            WritableSettingsStore settingsStore = manager.GetWritableSettingsStore(SettingsScope.UserSettings);
+
+            if (!settingsStore.CollectionExists(CollectionName))
+            {
+                settingsStore.CreateCollection(CollectionName);
+            }
+
+            foreach (PropertyInfo property in GetOptionProperties())
+            {
+                string output = SerializeValue(property.GetValue(this));
+                settingsStore.SetString(CollectionName, property.Name, output);
+            }
+
+            T liveModel = await GetInstanceAsync();
+
+            if (this != liveModel)
+            {
+                await liveModel.LoadAsync();
+            }
+        }
+
+        protected virtual string SerializeValue(object value)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, value);
+                stream.Flush();
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+
+        protected virtual object DeserializeValue(string value, Type type)
+        {
+            byte[] b = Convert.FromBase64String(value);
+
+            using (var stream = new MemoryStream(b))
+            {
+                var formatter = new BinaryFormatter();
+                return formatter.Deserialize(stream);
+            }
+        }
+
+        private static async Task<ShellSettingsManager> GetSettingsManagerAsync()
+        {
+            var svc = await AsyncServiceProvider.GlobalProvider.GetServiceAsync(typeof(SVsSettingsManager)) as IVsSettingsManager;
+
+            Assumes.Present(svc);
+            return new ShellSettingsManager(svc);
+        }
+
+        private IEnumerable<PropertyInfo> GetOptionProperties()
+        {
+            return GetType()
+                .GetProperties()
+                .Where(p => p.PropertyType.IsSerializable && p.PropertyType.IsPublic);
+        }
+    }
+}
---
+++ VSRAD.Syntax/Options/GeneralOptions.cs
@@ -0,0 +1,173 @@
+﻿using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static VSRAD.Syntax.Options.GeneralOptionPage;
+using Microsoft.VisualStudio.Settings;
+using System;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSRAD.Syntax.Options
+{
+    public class GeneralOptions : BaseOptionModel<GeneralOptions>
+    {
+        private const string InstructionCollectionName = "RadeonAsmInstructionCollection";
+        private static readonly Regex fileExtensionRegular = new Regex(@"^\.\w+$");
+        private readonly OptionsProvider _optionsProvider;
+
+        public GeneralOptions()
+        {
+            _optionsProvider = Package.Instance.GetMEFComponent<OptionsProvider>();
+        }
+
+        #region Options
+        [Category("Function list")]
+        [DisplayName("Function list default sort option")]
+        [Description("Set default sort option for Function List")]
+        public SortState SortOptions
+        {
+            get { return _optionsProvider.SortOptions; }
+            set { _optionsProvider.SortOptions = value; }
+        }
+
+        [Category("Function list")]
+        [DisplayName("Autoscroll function list")]
+        [Description("Scroll to current function in the function list automatically")]
+        public bool Autoscroll
+        {
+            get { return _optionsProvider.Autoscroll; }
+            set { _optionsProvider.Autoscroll = value; }
+        }
+
+        [Category("Syntax highlight")]
+        [DisplayName("Indent guide lines")]
+        [Description("Enable/disable indent guide lines")]
+        public bool IsEnabledIndentGuides
+        {
+            get { return _optionsProvider.IsEnabledIndentGuides; }
+            set { _optionsProvider.IsEnabledIndentGuides = value; }
+        }
+
+        [Category("Syntax file extensions")]
+        [DisplayName("Asm1 file extensions")]
+        [Description("List of file extensions for the asm1 syntax")]
+        public string Asm1FileExtensions
+        {
+            get { return ConvertExtensionsTo(_optionsProvider.Asm1FileExtensions); }
+            set { var extensions = ConvertExtensionsFrom(value); if (ValidateExtensions(extensions)) _optionsProvider.Asm1FileExtensions = extensions; }
+        }
+
+        [Category("Syntax file extensions")]
+        [DisplayName("Asm2 file extensions")]
+        [Description("List of file extensions for the asm2 syntax")]
+        public string Asm2FileExtensions
+        {
+            get { return ConvertExtensionsTo(_optionsProvider.Asm2FileExtensions); }
+            set { var extensions = ConvertExtensionsFrom(value); if (ValidateExtensions(extensions)) _optionsProvider.Asm2FileExtensions = extensions; }
+        }
+
+        [Category("Syntax instruction folder paths")]
+        [DisplayName("Instruction folder paths")]
+        [Description("List of folder path separated by semicolon wit assembly instructions with .radasm file extension")]
+        [Editor(typeof(FolderPathsEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        public string InstructionsPaths
+        {
+            get { return _optionsProvider.InstructionsPaths; }
+            set { _optionsProvider.InstructionsPaths = value; }
+        }
+
+        [Category("Autocompletion")]
+        [DisplayName("Instruction autocompletion")]
+        [Description("Autocomplete instructions")]
+        public bool AutocompleteInstructions
+        {
+            get { return _optionsProvider.AutocompleteInstructions; }
+            set { _optionsProvider.AutocompleteInstructions = value; }
+        }
+
+        [Category("Autocompletion")]
+        [DisplayName("Function autocompletion")]
+        [Description("Autocomplete function name")]
+        public bool AutocompleteFunctions
+        {
+            get { return _optionsProvider.AutocompleteFunctions; }
+            set { _optionsProvider.AutocompleteFunctions = value; }
+        }
+
+        [Category("Autocompletion")]
+        [DisplayName("Label autocompletion")]
+        [Description("Autocomplete labels")]
+        public bool AutocompleteLabels
+        {
+            get { return _optionsProvider.AutocompleteLabels; }
+            set { _optionsProvider.AutocompleteLabels = value; }
+        }
+
+        [Category("Autocompletion")]
+        [DisplayName("Variable autocompletion")]
+        [Description("Autocomplete global variables, local variables, function arguments")]
+        public bool AutocompleteVariables
+        {
+            get { return _optionsProvider.AutocompleteVariables; }
+            set { _optionsProvider.AutocompleteVariables = value; }
+        }
+        #endregion
+
+        public override async Task LoadAsync()
+        {
+            await base.LoadAsync();
+
+            var settingsManager = await _settingsManager.GetValueAsync();
+            var userSettingsStore = settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+
+            if (!userSettingsStore.CollectionExists(InstructionCollectionName))
+                return;
+
+            InstructionsPaths = userSettingsStore.PropertyExists(InstructionCollectionName, nameof(InstructionsPaths))
+                ? userSettingsStore.GetString(InstructionCollectionName, nameof(InstructionsPaths))
+                : OptionsProvider.GetDefaultInstructionDirectoryPath();
+        }
+
+        public override async Task SaveAsync()
+        {
+            await base.SaveAsync();
+
+            var settingsManager = await _settingsManager.GetValueAsync();
+            var userSettingsStore = settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+
+            if (!userSettingsStore.CollectionExists(InstructionCollectionName))
+                userSettingsStore.CreateCollection(InstructionCollectionName);
+
+            if (!InstructionsPaths.Equals(
+                OptionsProvider.GetDefaultInstructionDirectoryPath(),
+                StringComparison.OrdinalIgnoreCase))
+            {
+                userSettingsStore.SetString(InstructionCollectionName, nameof(InstructionsPaths), InstructionsPaths);
+            }
+        }
+
+        private static List<string> ConvertExtensionsFrom(string str) =>
+            str.Split(';').Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+        private static string ConvertExtensionsTo(IReadOnlyList<string> extensions) =>
+            string.Join(";", extensions.ToArray());
+
+        private static bool ValidateExtensions(List<string> extensions)
+        {
+            var sb = new StringBuilder();
+            foreach (var ext in extensions)
+            {
+                if (!fileExtensionRegular.IsMatch(ext))
+                    sb.AppendLine($"Invalid file extension format \"{ext}\"");
+            }
+            if (sb.Length != 0)
+            {
+                sb.AppendLine();
+                sb.AppendLine("Format example: .asm");
+                return false;
+            }
+            return true;
+        }
+    }
+}
