0b978d9438407fa9dc4207c8eaf36f1a7748b9cc
johnny-keker <isarzhevsky@gmail.com>
2021-09-07 21:01:49 +0300

    misplaced annotation fix

--- VSRAD.Package/Options/DebuggerOptions.cs
+++ VSRAD.Package/Options/DebuggerOptions.cs
@@ -56,14 +56,14 @@ namespace VSRAD.Package.Options
         private BreakMode _breakMode;
         public BreakMode BreakMode { get => _breakMode; set => SetField(ref _breakMode, value); }
 
+        private bool _forceOppositeTab = true;
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         [DefaultValue(true)]
-        private bool _forceOppositeTab = true;
         public bool ForceOppositeTab { get => _forceOppositeTab; set => SetField(ref _forceOppositeTab, value); }
         
+        private bool _preserveActiveDoc = true;
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         [DefaultValue(true)]
-        private bool _preserveActiveDoc = true;
         public bool PreserveActiveDoc { get => _preserveActiveDoc; set => SetField(ref _preserveActiveDoc, value); }
 
         public DebuggerOptions() { }
