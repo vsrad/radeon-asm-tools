4305c4f9cf7e2a212b1b77ee16bbe482d5e221a2
Alexey Akimov <kikimych@gmail.com>
2022-09-16 13:59:49 +0300

    Added version checking in handshake during connection to debug server

--- VSRAD.Package/Server/CommunicationChannel.cs
+++ VSRAD.Package/Server/CommunicationChannel.cs
@@ -12,6 +12,10 @@ using VSRAD.DebugServer.IPC.Responses;
 using VSRAD.Package.Options;
 using VSRAD.Package.ProjectSystem;
 using Task = System.Threading.Tasks.Task;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using VSRAD.DebugServer.IPC.Commands;
 
 namespace VSRAD.Package.Server
 {
@@ -30,10 +34,24 @@ namespace VSRAD.Package.Server
         void ForceDisconnect();
     }
 
-    public sealed class ConnectionRefusedException : System.IO.IOException
+    public sealed class ConnectionRefusedException : IOException
     {
         public ConnectionRefusedException(ServerConnectionOptions connection) :
             base($"Unable to establish connection to a debug server at {connection}")
+        { }
+    }
+
+    public sealed class UnsupportedServerVersionException : IOException
+    {
+        public UnsupportedServerVersionException(ServerConnectionOptions connection, Version serverVersion) :
+            base($"The debug server on host {connection} is out of date and missing critical features. Please update it to the {serverVersion} or above version.")
+        { }
+    }
+
+    public sealed class UnsupportedDebuggerVersionException : IOException
+    {
+        public UnsupportedDebuggerVersionException(ServerConnectionOptions connection, Version serverVersion) :
+            base($"This extension is out of date and missing critical features to work with debug server on host {connection}. Please update extension to the {serverVersion} or above version.")
         { }
     }
 
@@ -51,7 +69,7 @@ namespace VSRAD.Package.Server
         public event Action ConnectionStateChanged;
         public ServerConnectionOptions ConnectionOptions =>
             _project.Options?.Connection ?? new ServerConnectionOptions("Remote address is not specified", 0);
-
+        private Version _extensionVersion;
         private ClientState _state = ClientState.Disconnected;
         public ClientState ConnectionState
         {
@@ -64,6 +82,7 @@ namespace VSRAD.Package.Server
         }
 
         private static readonly TimeSpan _connectionTimeout = new TimeSpan(hours: 0, minutes: 0, seconds: 5);
+        private static readonly Regex _extensionVersionRegex = new Regex(@".*\/(?<version>.*)\/RadeonAsmDebugger\.dll", RegexOptions.Compiled);
 
         private readonly OutputWindowWriter _outputWindowWriter;
         private readonly IProject _project;
@@ -81,10 +100,55 @@ namespace VSRAD.Package.Server
             _project = project;
             _project.RunWhenLoaded((options) =>
                 options.PropertyChanged += (s, e) => { if (e.PropertyName == nameof(options.ActiveProfile)) ForceDisconnect(); });
+            var match = _extensionVersionRegex.Match(typeof(IProject).Assembly.CodeBase);
+            if (!match.Success)
+                throw new Exception("Error while getting current extension version.");
+            _extensionVersion = new Version(match.Groups["version"].Value);
         }
 
         public Task<T> SendWithReplyAsync<T>(ICommand command) where T : IResponse =>
             SendWithReplyAsync<T>(command, tryReconnect: true);
+
+        private async Task TryProcessClientHandshakeAsync(TcpClient client)
+        {
+            var writer = new StreamWriter(client.GetStream(), Encoding.UTF8) { AutoFlush = true };
+            var reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+
+            // Send client version to server
+            //
+            await writer.WriteLineAsync(_extensionVersion.ToString()).ConfigureAwait(false);
+            // Obtain server version
+            //
+            var serverResponse = await reader.ReadLineAsync().ConfigureAwait(false);
+            Version serverVersion = null;
+            if (!Version.TryParse(serverResponse, out serverVersion))
+            {
+                // Inform server that client declines serve's version
+                //
+                await writer.WriteLineAsync(HandShakeStatus.ClientNotAccepted.ToString()).ConfigureAwait(false);
+                throw new UnsupportedServerVersionException(ConnectionOptions, Constants.MinimalRequiredServerVersion);
+            }
+
+            if (serverVersion.CompareTo(Constants.MinimalRequiredServerVersion) < 0)
+            {
+                // Inform client that server declines client's version
+                //
+                await writer.WriteLineAsync(HandShakeStatus.ClientNotAccepted.ToString()).ConfigureAwait(false);
+                throw new UnsupportedServerVersionException(ConnectionOptions, Constants.MinimalRequiredServerVersion);
+            }
+
+            // Inform client that server accepts client's version
+            //
+            await writer.WriteLineAsync(HandShakeStatus.ClientAccepted.ToString()).ConfigureAwait(false);
+
+            // Check if client accepts server version
+            //
+            if (await reader.ReadLineAsync().ConfigureAwait(false) != HandShakeStatus.ServerAccepted.ToString())
+            {
+                throw new UnsupportedDebuggerVersionException(ConnectionOptions, serverVersion);
+            }
+            return;
+        }
 
         private async Task<T> SendWithReplyAsync<T>(ICommand command, bool tryReconnect) where T : IResponse
         {
@@ -129,7 +193,7 @@ namespace VSRAD.Package.Server
             if (_remoteEnvironment == null)
             {
                 await EstablishServerConnectionAsync().ConfigureAwait(false);
-                var environment = await SendWithReplyAsync<EnvironmentVariablesListed>(new ListEnvironmentVariables());
+                var environment = await SendWithReplyAsync<EnvironmentVariablesListed>(new ListEnvironmentVariables()).ConfigureAwait(false);
                 _remoteEnvironment = environment.Variables;
             }
             return _remoteEnvironment;
@@ -148,20 +212,23 @@ namespace VSRAD.Package.Server
             if (_connection != null && _connection.Connected) return;
 
             ConnectionState = ClientState.Connecting;
-
+#pragma warning disable CA2000 // we save this client as a _connection later if handshake processed successfully
             var client = new TcpClient();
+#pragma warning restore CA2000 // Dispose objects before losing scope
             try
             {
                 using (var cts = new CancellationTokenSource(_connectionTimeout))
                 using (cts.Token.Register(() => client.Dispose()))
                 {
-                    await client.ConnectAsync(ConnectionOptions.RemoteMachine, ConnectionOptions.Port);
+                    await client.ConnectAsync(ConnectionOptions.RemoteMachine, ConnectionOptions.Port).ConfigureAwait(false);
+                    await TryProcessClientHandshakeAsync(client).ConfigureAwait(false);
                     _connection = client;
                     ConnectionState = ClientState.Connected;
                 }
             }
-            catch (Exception)
+            catch (Exception e) when (!(e is UnsupportedDebuggerVersionException) && !(e is UnsupportedServerVersionException))
             {
+                client.Dispose();
                 ConnectionState = ClientState.Disconnected;
                 throw new ConnectionRefusedException(ConnectionOptions);
             }
