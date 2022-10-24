3ef7df6cc265bfea2a3d2d5ba3cb341e2473f4eb
johnny-keker <isarzhevsky@gmail.com>
2022-10-24 16:39:08 +0200

    moved HostItem to separate file

--- VSRAD.Package/ProjectSystem/Profiles/TargetHostsEditor.xaml.cs
+++ VSRAD.Package/ProjectSystem/Profiles/TargetHostsEditor.xaml.cs
@@ -12,24 +12,6 @@ namespace VSRAD.Package.ProjectSystem.Profiles
 {
     public sealed partial class TargetHostsEditor : DialogWindow
     {
-        public sealed class HostItem : DefaultNotifyPropertyChanged
-        {
-            private string _host = "";
-            public string Host { get => _host; set => SetField(ref _host, value); }
-
-            private string _alias = "";
-            public string Alias { get => _alias; set => SetField(ref _alias, value); }
-
-            public bool UsedInActiveProfile { get; }
-
-            public HostItem(string host, bool usedInActiveProfile, string alias = "")
-            {
-                Alias = alias;
-                Host = host;
-                UsedInActiveProfile = usedInActiveProfile;
-            }
-        }
-
         public static bool TryParseHost(string input, out string formatted, out string hostname, out ushort port)
         {
             formatted = "";
---
+++ VSRAD.Package/Utils/TargetHosts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSRAD.Package.Utils
+{
+    public sealed class HostItem : DefaultNotifyPropertyChanged
+    {
+        private string _host = "";
+        public string Host { get => _host; set => SetField(ref _host, value); }
+
+        private string _alias = "";
+        public string Alias { get => _alias; set => SetField(ref _alias, value); }
+
+        public bool UsedInActiveProfile { get; }
+
+        public HostItem(string host, bool usedInActiveProfile, string alias = "")
+        {
+            Alias = alias;
+            Host = host;
+            UsedInActiveProfile = usedInActiveProfile;
+        }
+    }
+}
