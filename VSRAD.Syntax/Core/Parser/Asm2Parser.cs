329005c04318143dd8a06d6b7e765494572dfd99
timlathy <tim.labushev@gmail.com>
2023-01-12 18:49:42 +0100

    update asm2 identifier highlighting

--- VSRAD.Syntax/Core/Parser/Asm2Parser.cs
+++ VSRAD.Syntax/Core/Parser/Asm2Parser.cs
@@ -193,18 +193,9 @@ namespace VSRAD.Syntax.Core.Parser
                             currentBlock.AddToken(variableDefinition);
                         }
                     }
-                    else if (token.Type == RadAsm2Lexer.CLOSURE_IDENTIFIER)
+                    else if (token.Type == RadAsm2Lexer.IDENTIFIER || token.Type == RadAsm2Lexer.CLOSURE_IDENTIFIER)
                     {
-                        var tokenText = token.GetText(version).Substring(1); // remove first '#' symbol
-                        if (!TryAddReference(tokenText, token, currentBlock, version, definitionContainer, cancellation) &&
-                            !TryAddBuiltinFunctionReference(tokenText, token, currentBlock, version))
-                        {
-                            referenceCandidates.AddLast((tokenText, token, currentBlock));
-                        }
-                    }
-                    else if (token.Type == RadAsm2Lexer.IDENTIFIER)
-                    {
-                        var tokenText = token.GetText(version);
+                        var tokenText = token.GetText(version).TrimPrefix("#");
                         if (!TryAddInstruction(tokenText, token, currentBlock, version) &&
                             !TryAddReference(tokenText, token, currentBlock, version, definitionContainer, cancellation) &&
                             !TryAddBuiltinFunctionReference(tokenText, token, currentBlock, version))
