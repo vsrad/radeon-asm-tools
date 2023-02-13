1f0b27bbbd053008e0bf594d124a24d6e0f171b4
johnny-keker <isarzhevsky@gmail.com>
2023-02-13 08:57:20 +0100

    packed int sample test

--- VSRAD.PackageTests/DebugVisualizer/DataFormattingTests.cs
+++ VSRAD.PackageTests/DebugVisualizer/DataFormattingTests.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VSRAD.Package.DebugVisualizer;
+using VSRAD.Package.Utils;
 using Xunit;
+using Xunit.Extensions;
 
 namespace VSRAD.PackageTests.DebugVisualizer
 {
@@ -23,10 +26,20 @@ namespace VSRAD.PackageTests.DebugVisualizer
             0x_000_ba6e1
         };
 
-        [Fact]
-        public void FormatDwordTests()
+        static readonly string[] ExpectedHex32 = new string[]
         {
+            "0xdeadbeef", "0xc0ffee00", "0x000ff1ce", "0xc001f00d", "0xabbaabba",
+            "0x0600dbed", "0xaddedbee", "0xcaba66e0", "0xdead7ace", "0x000ba6e1"
+        };
 
-        }
+        public static IEnumerable<object[]> ProvideTestData =>
+            new List<object[]>
+            {
+                new object[] { new VariableInfo(VariableType.Hex, 32), ExpectedHex32 },
+            };
+
+        [Theory, MemberData(nameof(ProvideTestData))]
+        public void FormatDwordTests(VariableInfo info, string[] expected) =>
+            Assert.Equal(_data.Select(d => DataFormatter.FormatDword(info, d, 0, 0, true)), expected);
     }
 }
