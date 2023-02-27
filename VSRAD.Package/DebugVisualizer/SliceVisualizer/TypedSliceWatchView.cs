db0e27b559d4cd192b9d3b432ed1782be468f1a4
johnny-keker <isarzhevsky@gmail.com>
2023-02-27 15:22:35 +0100

    removed dead code from slice visualizer

--- VSRAD.Package/DebugVisualizer/SliceVisualizer/TypedSliceWatchView.cs
+++ VSRAD.Package/DebugVisualizer/SliceVisualizer/TypedSliceWatchView.cs
@@ -34,118 +34,12 @@ namespace VSRAD.Package.DebugVisualizer.SliceVisualizer
 
         public float GetRelativeValue(int row, int column, int word = 0)
         {
-            // TODO: reimplement for VariableInfo
-            return 0.0f;
-            //switch (_type)
-            //{
-            //    case VariableType.Uint:
-            //    case VariableType.Hex:
-            //    case VariableType.Bin:
-            //        return ((float)(_view[row, column] - _minValue.uintValue)) / (_maxValue.uintValue - _minValue.uintValue);
-            //    case VariableType.Int:
-            //        return ((float)((int)_view[row, column] - _minValue.intValue)) / (_maxValue.intValue - _minValue.intValue);
-            //    case VariableType.Float:
-            //        var floatValue = BitConverter.ToSingle(BitConverter.GetBytes(_view[row, column]), 0);
-            //        return (floatValue - _minValue.floatValue) / (_maxValue.floatValue - _minValue.floatValue);
-            //    case VariableType.Half:
-            //        floatValue = Half.ToFloat(BitConverter.ToUInt16(BitConverter.GetBytes(_view[row, column]), startIndex: word * 2));
-            //        return (floatValue - _minValue.floatValue) / (_maxValue.floatValue - _minValue.floatValue);
-            //}
-            //throw new NotImplementedException();
+            throw new NotImplementedException("Current watch representation do not work with Slice Visualizer");
         }
 
         private static void GetMinMaxValues(SliceWatchView view, VariableType info, out TypedWatchValue min, out TypedWatchValue max)
         {
-            min = new TypedWatchValue { floatValue = float.MinValue };
-            max = new TypedWatchValue { floatValue = float.MaxValue };
-            return;
-            // TODO: reimplement for VariableInfo
-            //switch (type)
-            //{
-            //    case VariableType.Uint:
-            //    case VariableType.Hex:
-            //    case VariableType.Bin:
-            //        uint umin =  uint.MaxValue;
-            //        uint umax = uint.MinValue;
-            //        for (int row = 0; row < view.RowCount; ++row)
-            //        {
-            //            for (int col = 0; col < view.ColumnCount; ++col)
-            //            {
-            //                uint value = view[row, col];
-            //                if (value < umin)
-            //                    umin = value;
-            //                if (value > umax)
-            //                    umax = value;
-            //            }
-            //        }
-            //        min = new TypedWatchValue { uintValue = umin };
-            //        max = new TypedWatchValue { uintValue = umax };
-            //        return;
-            //    case VariableType.Int:
-            //        int imin = int.MaxValue;
-            //        int imax = int.MinValue;
-            //        for (int row = 0; row < view.RowCount; ++row)
-            //        {
-            //            for (int col = 0; col < view.ColumnCount; ++col)
-            //            {
-            //                int value = (int)view[row, col];
-            //                if (value < imin)
-            //                    imin = value;
-            //                if (value > imax)
-            //                    imax = value;
-            //            }
-            //        }
-            //        min = new TypedWatchValue { intValue = imin };
-            //        max = new TypedWatchValue { intValue = imax };
-            //        return;
-            //    case VariableType.Float:
-            //        float fmin = float.MaxValue;
-            //        float fmax = float.MinValue;
-            //        for (int row = 0; row < view.RowCount; ++row)
-            //        {
-            //            for (int col = 0; col < view.ColumnCount; ++col)
-            //            {
-            //                float value = BitConverter.ToSingle(BitConverter.GetBytes(view[row, col]), 0);
-            //                if (float.IsNaN(value))
-            //                    continue;
-            //                if (value < fmin)
-            //                    fmin = value;
-            //                if (value > fmax)
-            //                    fmax = value;
-            //            }
-            //        }
-            //        min = new TypedWatchValue { floatValue = fmin };
-            //        max = new TypedWatchValue { floatValue = fmax };
-            //        return;
-            //    case VariableType.Half:
-            //        fmin = float.MaxValue;
-            //        fmax = float.MinValue;
-            //        for (int row = 0; row < view.RowCount; ++row)
-            //        {
-            //            for (int col = 0; col < view.ColumnCount; ++col)
-            //            {
-            //                byte[] bytes = BitConverter.GetBytes(view[row, col]);
-            //                float firstHalf = Half.ToFloat(BitConverter.ToUInt16(bytes, 0));
-            //                float secondHalf = Half.ToFloat(BitConverter.ToUInt16(bytes, 2));
-            //                if (!float.IsNaN(firstHalf))
-            //                {
-            //                    if (firstHalf < fmin)
-            //                        fmin = firstHalf;
-            //                    if (firstHalf > fmax)
-            //                        fmax = firstHalf;
-            //                }
-            //                if (!float.IsNaN(secondHalf))
-            //                {
-            //                    if (secondHalf < fmin)
-            //                        fmin = secondHalf;
-            //                    if (secondHalf > fmax)
-            //                        fmax = secondHalf;
-            //                }
-            //            }
-            //        }
-            //        min = new TypedWatchValue { floatValue = fmin };
-            //        max = new TypedWatchValue { floatValue = fmax };
-            //        return;
+            throw new NotImplementedException("Current watch representation do not work with Slice Visualizer");
         }
 
         [StructLayout(LayoutKind.Explicit)]
