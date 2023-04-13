e30a916e19d97b6d4c889e1f7c9ee21434dd0dbd
johnny-keker <isarzhevsky@gmail.com>
2023-04-13 03:36:23 +0200

    building string representations for columns range modes

---
+++ VSRAD.Package/Utils/VisibleColumnsRange.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSRAD.Package.Utils
+{
+    public enum SelectorType
+    {
+        First, Last, Custom
+    }
+
+    class VisibleColumnsRange
+    {
+        public SelectorType Type;
+        public int X;
+        public int Y;
+        public string Custom;
+
+        public VisibleColumnsRange(SelectorType type, int x, int y)
+        {
+            Type = type; X = x; Y = y;
+        }
+
+        public string GetRepresentation(uint groupSize, List<int> range = null)
+        {
+            if (Type == SelectorType.Custom) return Custom;
+
+            int cur = Type == SelectorType.First ? 0 : Y - X;
+            var sb = new StringBuilder();
+            while (cur < groupSize)
+            {
+                sb.Append($"{cur}-{cur+X-1}:");
+                if (range != null) range.AddRange(Enumerable.Range(cur, X));
+                cur += Y;
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ VSRAD.PackageTests/Utils/VisibleColumnsRangeTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using VSRAD.Package.Utils;
+using Xunit;
+
+namespace VSRAD.PackageTests.Utils
+{
+    public class VisibleColumnsRangeTests
+    {
+        [Fact]
+        public void TestString()
+        {
+            var range = new VisibleColumnsRange(SelectorType.First, 16, 64);
+            Assert.Equal("0-15:64-79:128-143:192-207:256-271:320-335:384-399:448-463:512-527:576-591:", range.GetRepresentation(640));
+
+            range = new VisibleColumnsRange(SelectorType.Last, 16, 64);
+            Assert.Equal("48-63:112-127:176-191:240-255:304-319:368-383:432-447:496-511:560-575:624-639:", range.GetRepresentation(640));
+        }
+    }
+}
