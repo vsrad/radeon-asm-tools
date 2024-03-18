3cd1dfcf18e49be1f768ee5295432cd4b7c16b75
timlathy <tim.labushev@gmail.com>
2024-03-18 09:13:05 +0100

    begin moving from ad-hoc reflection-based communication between extensions to a proper statically-typed solution via a shared library

---
+++ VSRAD.SyntaxPackageBridge/ISyntaxPackageBridge.cs
@@ -0,0 +1,10 @@
+﻿namespace VSRAD.SyntaxPackageBridge
+{
+    /// <summary>
+    /// Specifies the interface for inter-extension communication between VSRAD.Package and VSRAD.Syntax.
+    /// The interface is implemented by VSRAD.Package, and VSRAD.Syntax requests the implementation via MEF at runtime.
+    /// </summary>
+    public interface ISyntaxPackageBridge
+    {
+    }
+}
